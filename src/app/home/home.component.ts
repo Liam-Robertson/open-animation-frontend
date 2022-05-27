b6a4caca948a71adf8f91acfa4cc52a258bbcf2a
Liam-Robertson <liamjohnrobertson@gmail.com>
2022-05-28 00:02:51 +0100

    quality of life changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -209,7 +209,6 @@ export class HomeComponent implements OnInit {
         this.instructionsBool = true;
         (document.getElementById("instructions-button") as HTMLButtonElement).style.background =  "rgba(41, 169, 255, 0.473)";
         this.canvasBool = false;
-        this.initCanvasBool = false;
         this.ctx.canvas.hidden = true;
         this.feedbackBool = false;
         this.videoBool = false;
@@ -223,7 +222,6 @@ export class HomeComponent implements OnInit {
     if (!this.videoBool) {
       this.videoBool = true;
       this.canvasBool = false;
-      this.initCanvasBool = false;
       (document.getElementById("video-button") as HTMLButtonElement).style.background =  "rgba(41, 169, 255, 0.473)";
       this.instructionsBool = false;
       this.feedbackBool = false;
@@ -239,7 +237,6 @@ export class HomeComponent implements OnInit {
       this.feedbackBool = true;
       this.videoBool = false;
       this.canvasBool = false;
-      this.initCanvasBool = false;
       this.instructionsBool = false;
       this.ctx.canvas.hidden = true;
       (document.getElementById("video-button") as HTMLButtonElement).style.background =  "rgba(13, 29, 207, 0.048)";
